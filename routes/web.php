af28a8f2ddb912483af22eab36250aa549cc7fbd
Al-Giovinetti <giovinettialessio@gmail.com>
2023-08-22 16:09:08 +0200

    Rotta admin home + reindirizzo pagina da loggati

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function (){
+    Route::get('/home',[AdminHomeController::class,'home'])->name('home');
+});
